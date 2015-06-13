25741b0cde1fc82a7630da24e74ec1dc1402e8b7
zhaogang <zhaohub@126.com>
2015-06-13 18:28:49 +0800

    Update main.c
    update print_hello to print_s

--- main.c
+++ main.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
-void print_hello(void);
+void print_s(char *s);
 
 int main(int argc,char *argv[])
 {
-  print_hello();
+  char *s="character string";
+  print_s(s);
   return 0;
 }
 
-void print_hello(void)
+void print_s(char *s)
 {
   printf("hello world");
 }
